07c759ab47dcf0432731004a61d2084ac1cb817f
nguyen2234 <nguyen14012004@gmail.com>
2025-03-23 21:26:24 +0700

    Catagori and Contact

---
+++ Controllers/CatagoriController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace estore.Controllers
+{
+    public class CatagoriController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
